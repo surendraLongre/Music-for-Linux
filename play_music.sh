597363a04379bd824b51c495280fafe74e20f23a
Surendra Longre <murari.longre2366@gmail.com>
2024-06-19 14:31:18 +0530

    finish the integration of stop-music feature

--- play_music.sh
+++ play_music.sh
@@ -9,6 +9,25 @@ music_path="/home/kgpk/my_scripts/music_script/songs_link.txt"	# music text file
 song_name=$1
 
 link_addr="$(grep -i "$song_name" "$music_path" | awk '{print $NF}')"
+# add the stop-music option to stop mpv player
+
+if [ -z "$1" ]; # if the user don't provide appropriate command line options
+then
+	echo "usage: playmusic <song_name>	//to play the song, or"
+	echo "usage: playmusic stop-music 	//to stop the music"
+	exit 0
+
+elif [ "$1" = "stop-music" ];
+then
+	music_pid="$(ps -ef | grep mpv | grep '\--loop --no-video' | awk '{print $2}')"
+	kill $music_pid
+	exit 0
+fi
+
+#
+# play songs from url
+# get the url links and check whether it exists or not or play the music
+#
 
 if [[ -z $link_addr ]];
 then
@@ -37,14 +56,14 @@ else
 			if [[ -z $link_addr ]];
 			then
 				echo "song not found"
-				exit
+				exit 1
 			else
 				mpv $link_addr --no-video &>/dev/null &
 				echo "playing $song_name"
 				wait
 			fi
 		done < "$music_path"
-		exit
+		exit 0
 	fi
 
 fi
