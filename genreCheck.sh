adcd447376f8135b3546992db5fe41ad0f1ba365
Surendra Longre <murari.longre2366@gmail.com>
2024-06-19 22:50:10 +0530

    finish script to check whether songs have genre or not

---
+++ genreCheck.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#write script to make if they don't include any genre
+
+music_dir='/media/kgpk/Music/'
+
+while read line; do
+	genre_type="$(exiftool -genre "$music_dir$line" | egrep -i "romantic|sad|uplifting" )"
+	if [ -z "$genre_type" ];
+	then
+		echo "$line"
+	fi
+#	echo "$line"
+ #	echo "$file_name"
+#done <<< "$(ls $music_dir | xargs -I {} echo '{}')" 
+done < <(ls $music_dir) 
