1c3936353488446a7baebefb97c609171fbd3e44
Surendra Longre <murari.longre2366@gmail.com>
2024-06-26 13:56:48 +0530

    finish scripting songsWithGenre file

---
+++ songsWithGenre.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#write script to output files with particular genre
+
+music_dir='/media/kgpk/Music/'
+
+while read line; do
+	genre_type="$(exiftool -genre "$music_dir$line" | grep -i "$1" )"
+	if [ -z "$genre_type" ];
+	then
+		continue
+	fi
+	echo "$line"
+ #	echo "$file_name"
+done < <(ls $music_dir) 
