2efe00b124f7dc8a2d9fdc93e67c14a8d493d841
Surendra Longre <murari.longre2366@gmail.com>
2024-07-18 21:07:27 +0530

    fix bug: make script to play genre songs

--- play.sh
+++ play.sh
@@ -52,7 +52,7 @@ then
 		then
 			continue
 		else
-			play "$link_addr" --no-video ${@:2} --sub-file-paths="$lyric_dir" --sub-auto=fuzzy
+			play "$dir" --no-video ${@:2} --sub-file-paths="$lyric_dir" --sub-auto=fuzzy
 			#wait
 		fi
 	done < <(ls $music_dir | shuf )
